1a64d41b271a28f7d3918d50c532c6e8153b2631
Doug <doug.a.akridge@gmail.com>
2020-05-09 06:48:54 -0400

    create a default nodes list

--- src/storage-fns/nodes.js
+++ src/storage-fns/nodes.js
@@ -1,64 +1,78 @@
 // dependencies
 import path from "path";
-import isIP from 'is-ip';
-import fse from 'fs-extra';
+import isIP from "is-ip";
+import fse from "fs-extra";
 
 // get current directory
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 // configuration
 const FILE_LOCATION = path.join(__dirname, "../../storage/nodes-list");
+const DEFAULT_NODES_LOCATION = path.join(
+  __dirname,
+  "../../storage/default-nodes"
+);
 
 const getFilePath = async () => {
-    const filePath = FILE_LOCATION;
-    await fse.ensureFile(filePath);
+  const filePath = FILE_LOCATION;
+  await fse.ensureFile(filePath);
 
-    return filePath;
-}
+  return filePath;
+};
 
 const isValidIP = (value) => {
-    try {
-        const [ip, port] = value.split(':');
-        return isIP(ip);
-    }
-    catch (ermahgerd) {
-        // pass
-    }
+  try {
+    const [ip, port] = value.split(":");
+    return isIP(ip);
+  } catch (ermahgerd) {
+    // pass
+  }
 
-    return false;
-}
+  return false;
+};
 
 export const getNodes = async () => {
-    let nodes = [];
-    const filePath = await getFilePath();
+  let nodes = [];
+  let defaultNodes = [];
+  const filePath = await getFilePath();
 
-    try {
-        nodes = await fse.readJson(filePath);
-    }
-    catch (ermahgerd) {
-        nodes = [];
-    }
+  try {
+    defaultNodes = await fse.readJson(DEFAULT_NODES_LOCATION);
+  } catch (ermahgerd) {
+    defaultNodes = [];
+  }
 
-    return nodes.filter(item => { return isValidIP(item); });
-}
+  try {
+    nodes = await fse.readJson(filePath);
+  } catch (ermahgerd) {
+    nodes = [];
+  }
+
+  // make a list of all nodes
+  const allNodes = [...defaultNodes, ...nodes];
+
+  return allNodes.filter((item) => {
+    return isValidIP(item);
+  });
+};
 
 export const insertNode = async (newNodeAddress) => {
-    const nodes = await getNodes();
-    const filePath = await getFilePath();
-    const nodeExists = nodes.includes(newNodeAddress);
+  const nodes = await getNodes();
+  const filePath = await getFilePath();
+  const nodeExists = nodes.includes(newNodeAddress);
 
-    if (!nodeExists) {
-        nodes.push(newNodeAddress);
-        await fse.writeJson(filePath, chain, { spaces: 2 });
-
-        return {
-            nodeAdded: true,
-            nodeCount: nodes.length + 1,
-        };
-    }
+  if (!nodeExists) {
+    nodes.push(newNodeAddress);
+    await fse.writeJson(filePath, chain, { spaces: 2 });
 
     return {
-        nodeAdded: false,
-        nodeCount: nodes.length,
+      nodeAdded: true,
+      nodeCount: nodes.length + 1,
     };
-}
+  }
+
+  return {
+    nodeAdded: false,
+    nodeCount: nodes.length,
+  };
+};
